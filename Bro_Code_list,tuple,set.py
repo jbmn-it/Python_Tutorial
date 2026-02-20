ca434d17cbd9db2ff9641f6e0db7d8b3737fc188
jkbin <barmonjoy06@gmail.com>
2026-02-20 18:47:25 +0600

    Bro_Code

---
+++ Bro_Code_list,tuple,set.py
@@ -0,0 +1,43 @@
+# collection = single "variable" used to stote multiple values
+#   list = [] ordered and changeable. Duplicates OK
+#   set = {} unordered and immutable, but Add/Remove OK. NO dublicates
+#   tuple= () ordered and unchangeable. Duplicates OK. Faster
+
+# fruits = ("Apple", "Orange", "Banana", "Coconut", "Pineapple", "Mango", "Apple")
+
+# list
+
+# print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("Mango" in fruits)
+
+# fruits[1] = "lychee"
+# fruits.append("Jack-fruit")
+# fruits.remove("Apple")
+# fruits.insert(0, "Guava")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# print(fruits.index("Mango"))
+# print(fruits.count("Apple"))
+
+# print(fruits)
+# for fruit in fruits:
+#     print(fruit)
+
+# set
+
+# print("Guava" in fruits)
+# fruits.add("apple")
+# fruits.pop()
+# print(fruits)
+
+# tuple
+
+# print(fruits.count("Apple"))
+# print(fruits.index("Mango"))
+# print(dir(fruits))
+# for fruit in fruits:
+#     print(fruit)
+  
